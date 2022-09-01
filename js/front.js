682db7e1b26b328d4c9632b024d173fcde06d92b
Etuhu <andgiz@mail.ru>
2022-09-01 15:53:35 +0700

    Доработана инициализация сторонних библиотек относительно fullpage.js

--- js/front.js
+++ js/front.js
@@ -1,44 +1,69 @@
 window.addEventListener("DOMContentLoaded", function () {
-	//Инициализация слайда с parallax
-	// if ($(window).width() > 991) {
-	let scene = document.getElementById("parallax-there-is");
-	let parallaxInstance = new Parallax(scene, {
-		relativeInput: true,
-		clipRelativeInput: true,
-		recordHistory: true,
-		limitX: 150,
-		limitY: 200,
-		scalarX: 7,
-		scalarY: 7,
-	});
 
-	let smokeWrapperScene = document.getElementById("parallax-smoke-wrapper");
-	let parallaxSmokeWrapper = new Parallax(smokeWrapperScene, {
-		relativeInput: true,
-		hoverOnly: true,
-		clipRelativeInput: true,
-		recordHistory: true,
-		limitX: 150,
-		limitY: 200,
-		scalarX: 7,
-		scalarY: 7,
-	});
+	//Добавление и удаление класса для старта анимаций элементов активного раздела
+	var addAnimatedClass = function (el) {
+		if (!el.item.classList.contains("animated")) {
+			el.item.classList.add("animated");
+		}
+	};
+	var removeAnimatedClass = function (el) {
+		if (el.item.classList.contains("animated")) {
+			el.item.classList.remove("animated");
+		}
+	};
 
-	let specialOfferBg = document.getElementById("parallax-specialOfferBg");
-	let parallaxspecialOfferBg = new Parallax(specialOfferBg, {
-		relativeInput: true,
-		hoverOnly: true,
-		clipRelativeInput: true,
+	//Инициализация Fullpage-блока
+	new fullpage("#fullpage", {
+		licenseKey: null,
+		autoScrolling: true,
+		scrollHorizontally: true,
+		verticalCentered: false,
+		scrollingSpeed: 700,
+		scrollOverflow: true,
+		// responsiveWidth: 992,
+		// responsiveHeight: 0,
+		lazyLoading: true,
 		recordHistory: true,
-		limitX: 150,
-		limitY: 300,
-		scalarX: 7,
-		scalarY: 10,
-	});
-	// }
+		observer: true,
+		credits: { enabled: false, label: "Made with fullPage.js", position: "right" },
+		afterRender: function(){
+			//Инициализация слайда с parallax
+			// if ($(window).width() > 991) {
+			let scene = document.getElementById("parallax-there-is");
+			new Parallax(scene, {
+				relativeInput: true,
+				clipRelativeInput: true,
+				recordHistory: true,
+				limitX: 150,
+				limitY: 200,
+				scalarX: 7,
+				scalarY: 7,
+			});
 
-	(function($) {
-		$(document).ready(function() {
+			let smokeWrapperScene = document.getElementById("parallax-smoke-wrapper");
+			new Parallax(smokeWrapperScene, {
+				relativeInput: true,
+				hoverOnly: true,
+				clipRelativeInput: true,
+				recordHistory: true,
+				limitX: 150,
+				limitY: 200,
+				scalarX: 7,
+				scalarY: 7,
+			});
+
+			let specialOfferBg = document.getElementById("parallax-specialOfferBg");
+			new Parallax(specialOfferBg, {
+				relativeInput: true,
+				hoverOnly: true,
+				clipRelativeInput: true,
+				recordHistory: true,
+				limitX: 150,
+				limitY: 300,
+				scalarX: 7,
+				scalarY: 10,
+			});
+			// }
 			$.jScrollability([
 				{
 					'selector': '#redTitle',
@@ -101,177 +126,141 @@ window.addEventListener("DOMContentLoaded", function () {
 					}
 				},
 			]);
+		},
+		onLeave: function(origin, destination, direction, trigger){
+			addAnimatedClass(destination);
+
+			if (destination.isFirst) {
+				showHeader();
+				if (!header.classList.contains("fixed-visible")) {
+					header.classList.add("fixed-visible");
+				}
+				if (scrollButton.classList.contains("show")) {
+					scrollButton.classList.remove("show");
+				}
+			} else {
+				if (header.classList.contains("fixed-visible")) {
+					header.classList.remove("fixed-visible");
+				}
+				if (!scrollButton.classList.contains("show")) {
+					scrollButton.classList.add("show");
+				}
+				hiddenHeader();
+				mouseMoveToTop();
+			}
+		},
+		afterLoad: function(origin, destination, direction, trigger){
+			addAnimatedClass(origin);
+		},
+	});
+
+	//Появление и исчезнование шапки сайта
+	let header = document.querySelector(".header");
+
+	let hiddenHeader = function () {
+		if (!header.classList.contains("hidden")) {
+			header.classList.add("hidden");
+		}
+	};
+
+	let showHeader = function () {
+		if (header.classList.contains("hidden")) {
+			header.classList.remove("hidden");
+		}
+	};
+
+	//Появление шапки при наведении на верхнюю часть окна и ее отключение при уходе в другую зону
+	function mouseMoveToTop () {
+		window.addEventListener("mousemove", function (evt) {
+			if (evt.clientY <= header.clientHeight) {
+				showHeader();
+			} else if (evt.clientY > header.clientHeight) {
+				hiddenHeader();
+			}
 		});
-	})(jQuery);
-});
+	};
+
+	//Кнопка прокрутки наверх
+	var scrollButton = document.getElementById('top-button');
+	scrollButton.addEventListener("click", function () {
+		fullpage_api.moveTo(1);
+	});
 
 
-var addAnimatedClass = function (el) {
-	if (!el.item.classList.contains("animated")) {
-		el.item.classList.add("animated");
-	}
-};
-
-var removeAnimatedClass = function (el) {
-	if (el.item.classList.contains("animated")) {
-		el.item.classList.remove("animated");
-	}
-};
+	//Переключение изображения с дымом в зависимости от выбранного цвета
+	let refreshSmokeColor = function (sliderName) {
+		var colorSmokes = document.querySelectorAll(".smoke-wrapper-color > img");
+		colorSmokes.forEach((colorSmoke) => {
+			colorSmoke.style.opacity = 0;
+		});
+		var colorSmokeItem = document.querySelector(`.smoke-wrapper-color > img[data-color=${sliderName.slides[sliderName.activeIndex].dataset.color}]`);
+		colorSmokeItem.style.opacity = 1;
+	};
 
 
-//Инициализация Fullpage-блока
-const fullPage = new fullpage("#fullpage", {
-	licenseKey: null,
-	autoScrolling: true,
-	scrollHorizontally: true,
-	verticalCentered: false,
-	scrollingSpeed: 700,
-	scrollOverflow: true,
-	// responsiveWidth: 992,
-	// responsiveHeight: 0,
-	lazyLoading: true,
-	recordHistory: true,
-	observer: true,
-	credits: { enabled: false, label: "Made with fullPage.js", position: "right" },
-	// beforeLeave: function(origin, destination, direction, trigger){
-	// 	if (direction =='up') {
-	// 		setTimeout(removeAnimatedClass, 400, origin);
-	// 	};
-	// },
-	onLeave: function(origin, destination, direction, trigger){
-		// var origin = this;
-		addAnimatedClass(destination);
+	//Слайдер с выбором цвета продукта
+	new Swiper(".choose-swiper", {
+		slidesPerView: 5,
+		centeredSlides: true,
+		spaceBetween: 5,
+		slidesPerGroup: 1,
+		loop: true,
+		watchOverflow: true,
+		watchSlidesProgress: true,
+		initialSlide: 2,
+		slideToClickedSlide: true,
+		simulateTouch: true,
+		observer: true,
+		pagination: {
+			el: ".choose-swiper-pagination",
+			clickable: true,
+			renderBullet: function (index, className) {
+				var slideColors = this.slides;
+				var colors = [];
+				slideColors.forEach((slideColor) => {
+					colors.push(slideColor.dataset.color);
+				});
+				var filterColors = colors.reduce((result, item) => {
+					return result.includes(item) ? result : [...result, item];
+				}, []);
+				return '<span class="' + className + " " + filterColors[index] + '">' + filterColors[index] + "</span>";
+			},
+		},
+		breakpoints: {
+			1600: {
+				slidesPerView: 5,
+				slidesPerGroup: 1,
+			},
+			1300: {
+				slidesPerView: 5,
+				slidesPerGroup: 1,
+			},
+			1100: {
+				slidesPerView: 4,
+				slidesPerGroup: 1,
+			},
+			992: {
+				slidesPerView: 4,
+				slidesPerGroup: 1,
+			},
+			768: {
+				slidesPerView: 3,
+				slidesPerGroup: 1,
+			},
 
-		if (destination.isFirst) {
-			showHeader();
-			if (!header.classList.contains("fixed-visible")) {
-				header.classList.add("fixed-visible");
-			}
-			if (scrollButton.classList.contains("show")) {
-				scrollButton.classList.remove("show");
-			}
-		} else {
-			if (header.classList.contains("fixed-visible")) {
-				header.classList.remove("fixed-visible");
-			}
-			if (!scrollButton.classList.contains("show")) {
-				scrollButton.classList.add("show");
-			}
-			hiddenHeader();
-			mouseMoveToTop();
-		}
-	},
-	afterLoad: function(origin, destination, direction, trigger){
-		// var origin = this;
-		addAnimatedClass(origin);
-	},
-});
-
-//Появление и исчезнование шапки сайта
-let header = document.querySelector(".header");
-
-let hiddenHeader = function () {
-	if (!header.classList.contains("hidden")) {
-		header.classList.add("hidden");
-	}
-};
-
-let showHeader = function () {
-	if (header.classList.contains("hidden")) {
-		header.classList.remove("hidden");
-	}
-};
-
-//Появление шапки при наведении на верхнюю часть окна и ее отключение при уходе в другую зону
-function mouseMoveToTop () {
-	window.addEventListener("mousemove", function (evt) {
-		if (evt.clientY <= header.clientHeight) {
-			showHeader();
-		} else if (evt.clientY > header.clientHeight) {
-			hiddenHeader();
+			0: {
+				spaceBetween: 5,
+				slidesPerView: 2,
+				slidesPerGroup: 1,
+			},
+		},
+		on: {
+			init: function () {
+				refreshSmokeColor(this);
+			},
+			slideChange: function () {
+				refreshSmokeColor(this);
+			},
 		}
 	});
-};
-
-//Кнопка прокрутки наверх
-var scrollButton = document.getElementById('top-button');
-scrollButton.addEventListener("click", function () {
-	fullpage_api.moveTo(1);
-});
-
-
-//Переключение изображения с дымом в зависимости от выбранного цвета
-let refreshSmokeColor = function (sliderName) {
-	var colorSmokes = document.querySelectorAll(".smoke-wrapper-color > img");
-	colorSmokes.forEach((colorSmoke) => {
-		colorSmoke.style.opacity = 0;
-	});
-	var colorSmokeItem = document.querySelector(`.smoke-wrapper-color > img[data-color=${sliderName.slides[sliderName.activeIndex].dataset.color}]`);
-	colorSmokeItem.style.opacity = 1;
-};
-
-const swiperChoose = new Swiper(".choose-swiper", {
-	slidesPerView: 5,
-	centeredSlides: true,
-	spaceBetween: 5,
-	slidesPerGroup: 1,
-	loop: true,
-	watchOverflow: true,
-	watchSlidesProgress: true,
-	initialSlide: 2,
-	slideToClickedSlide: true,
-	simulateTouch: true,
-	observer: true,
-	// observeSlideChildren: true,
-	pagination: {
-		el: ".choose-swiper-pagination",
-		clickable: true,
-		renderBullet: function (index, className) {
-			var slideColors = this.slides;
-			var colors = [];
-			slideColors.forEach((slideColor) => {
-				colors.push(slideColor.dataset.color);
-			});
-			var filterColors = colors.reduce((result, item) => {
-				return result.includes(item) ? result : [...result, item];
-			}, []);
-			return '<span class="' + className + " " + filterColors[index] + '">' + filterColors[index] + "</span>";
-		},
-	},
-	breakpoints: {
-		1600: {
-			slidesPerView: 5,
-			slidesPerGroup: 1,
-		},
-		1300: {
-			slidesPerView: 5,
-			slidesPerGroup: 1,
-		},
-		1100: {
-			slidesPerView: 4,
-			slidesPerGroup: 1,
-		},
-		992: {
-			slidesPerView: 4,
-			slidesPerGroup: 1,
-		},
-		768: {
-			slidesPerView: 3,
-			slidesPerGroup: 1,
-		},
-
-		0: {
-			spaceBetween: 5,
-			slidesPerView: 2,
-			slidesPerGroup: 1,
-		},
-	},
-	on: {
-		init: function () {
-			refreshSmokeColor(this);
-		},
-		slideChange: function () {
-			refreshSmokeColor(this);
-		},
-	  }
 });
